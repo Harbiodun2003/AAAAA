6d9fbacef3cfb3ed33c9a0b1248b7f0a5517382f
Fuadoluwaniyi <fuadoluwaniyi@gmail.com>
2023-03-31 17:02:35 +0100

    first

---
+++ sitescript.js
@@ -0,0 +1,18 @@
+
+
+
+var mydate = new Date;
+
+const time = document.getElementById("timeMachine");
+time.style.color="red";
+time.innerHTML=mydate.toUTCString();
+    var loader = document.getElementById("loader");
+    window.addEventListener("loadstart", function(){
+     })
+    window.addEventListener("load", function(){
+   
+     loader.style.display ="none";
+    })
+
+
+
